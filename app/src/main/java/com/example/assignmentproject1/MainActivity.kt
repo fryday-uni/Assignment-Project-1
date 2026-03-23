270394aa56fb3fc4b7b56b2f575ff2914b82e398
chip <ST1053444015@myemeris.za>
2026-03-23 15:26:04 +0200

    first app with when function logic working

--- app/src/main/java/com/example/assignmentproject1/MainActivity.kt
+++ app/src/main/java/com/example/assignmentproject1/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.example.assignmentproject1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    enum class Time {
+        MORNING,
+        MID_MORNING,
+        AFTERNOON,
+        AFTERNOON_SNACK_TIME,
+        DINNER,
+        AFTER_DINNER,
+        NIGHT,
+        EVENING,
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // declaring the variables and connecting them to respective buttons
+        val currentTime = findViewById<EditText>(R.id.timeEditText)
+        val sparkResult = findViewById<TextView>(R.id.resultView)
+        val printTextView = findViewById<Button>(R.id.sparkButton)
+        val clearButton = findViewById<Button>(R.id.clearButton)
+
+
+        printTextView.setOnClickListener {
+            // user input of time set to uppercase to make it not case-sensitive
+            val timeInput = currentTime.text.toString().uppercase()
+
+            // changing this stupid string into enum value, stole from ChatGPT
+            val time = try {
+                Time.valueOf(timeInput)
+            } catch (e: Exception) {
+                null
+            }
+
+            //sets the spark suggestion depending on time
+            val sparkSuggestion = when(time) {
+                Time.MORNING -> "Send a good morning text to a family member. "
+                Time.MID_MORNING -> "Reach out to a colleague with a quick thank you"
+                Time.AFTERNOON -> "Share a funny meme or interesting link with a friend"
+                Time.AFTERNOON_SNACK_TIME -> "send a quick 'thinking of you' message"
+                Time.DINNER,Time.EVENING -> "call a friend or relative for a 5-minute catchup"
+                Time.AFTER_DINNER,Time.NIGHT -> "Leave a thoughtful comment on a friends's post"
+                else -> "Please put _ instead of space "
+            }
+
+            sparkResult.text = sparkSuggestion
+
         }
+        //makes the button clear both
+        clearButton.setOnClickListener {
+            sparkResult.text = ""
+            currentTime.text.clear()
+
+
+        }
+
     }
 }
