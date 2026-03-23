2122e5d921e8fe1ee7988f0c21f00129fca4da95
chip <ST1053444015@myemeris.za>
2026-03-23 18:20:18 +0200

    Sunglasses and delay working

--- app/src/main/java/com/example/assignmentproject1/Welcome.kt
+++ app/src/main/java/com/example/assignmentproject1/Welcome.kt
@@ -1,8 +1,11 @@
 package com.example.assignmentproject1
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,10 +19,21 @@ class Welcome : AppCompatActivity() {
 
         val welcomeButton = findViewById<Button>(R.id.button)
 
+        // trying to make the sun smile, image is off and is turned on by the coninue button
+        val sunSmile = findViewById<ImageView>(R.id.imageGlasses)
+        val sunGlasses = findViewById<ImageView>(R.id.imageSun)
+
         welcomeButton.setOnClickListener {
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            sunSmile.visibility = View.VISIBLE  //turning the image on
+
+
+           //first waits 2 seconds
+            welcomeButton.postDelayed({
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            },2000) //then moves on to next activity
 
         }
     }
